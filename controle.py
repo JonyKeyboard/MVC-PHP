14adfa2a2c8ddafb996da92edddf438920bc90e9
Stanley Jony <1jony2jony@gmail.com>
2022-01-20 19:17:06 -0300

    Insert dados no DB

--- controle.py
+++ controle.py
@@ -1,13 +1,34 @@
 from PyQt5 import  uic,QtWidgets
+import mysql.connector  
+
+numero_id = 0
+
+banco = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="cadastro_recibos"
+)
 
 def funcao_principal():
     nome = formulario.lineEditNome.text()
-    valor = formulario.doubleSpinBox.text()
+    preco = formulario.doubleSpinBox.text().replace(',' , '.')
     descricao = formulario.lineEdidDesc.text()
 
     print("Nome:",nome)
-    print("Valor:",valor)
+    print("Preco:",preco)
     print("Descrição:",descricao)
+    
+
+    cursor = banco.cursor()
+    comando_SQL = "INSERT INTO recibos (descricao,preco,nome) VALUES (%s,%s,%s)"
+    dados = (str(descricao),str(preco),str(nome))
+    cursor.execute(comando_SQL,dados)
+    banco.commit()
+    formulario.lineEditNome.setText("")
+    formulario.doubleSpinBox.clear()
+    formulario.lineEdidDesc.setText("")
+    
     
 
 app=QtWidgets.QApplication([])
