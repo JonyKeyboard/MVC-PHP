745325201f61a3f6f7815f222d4742e57fb60745
Stanley Jony <1jony2jony@gmail.com>
2022-01-31 15:59:05 -0300

    Add Listagem de Recibos

--- controle.py
+++ controle.py
@@ -21,19 +21,35 @@ def funcao_principal():
     
 
     cursor = banco.cursor()
-    comando_SQL = "INSERT INTO recibos (descricao,preco,nome) VALUES (%s,%s,%s)"
-    dados = (str(descricao),str(preco),str(nome))
+    comando_SQL = "INSERT INTO recibos (nome,preco,descricao) VALUES (%s,%s,%s)"
+    dados = (str(nome),str(preco),str(descricao))
     cursor.execute(comando_SQL,dados)
     banco.commit()
     formulario.lineEditNome.setText("")
     formulario.doubleSpinBox.clear()
     formulario.lineEdidDesc.setText("")
-    
-    
+
+def chama_segunda_tela():
+    segunda_tela.show()
+
+    cursor = banco.cursor()
+    comando_SQL = "SELECT nome, preco, descricao FROM recibos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+
+    segunda_tela.tableWidget.setRowCount(len(dados_lidos))
+    segunda_tela.tableWidget.setColumnCount(3)
+   
+    for i in range(0, len(dados_lidos)):
+       for j in range(0, 3):
+           segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+
 
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("formulario.ui")
+segunda_tela=uic.loadUi("listar_dados.ui")
 formulario.ButtonGerar.clicked.connect(funcao_principal)
+formulario.ButtonGerados.clicked.connect(chama_segunda_tela)
 
 formulario.show()
 app.exec()
