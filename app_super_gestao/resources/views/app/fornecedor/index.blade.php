9afa65d9c036ad7c20b070c4d4423844592c82a6
Stanley <1jony2jony@gmail.com>
2022-08-18 10:01:30 -0300

    Seção 7: blade @isset e @ empty

---
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,17 @@
+<h3>Fornecedor</h3>
+
+@isset($fornecedores)
+
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+    @endisset
+
+
+@endisset
