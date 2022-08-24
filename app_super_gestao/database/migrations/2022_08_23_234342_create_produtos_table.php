cb33fb7b0e1d2f576b0b918817dc8ed2df41884b
Stanley Jony <1jony2jony@gmail.com>
2022-08-23 21:01:15 -0300

    Entendendo up,down,nullable e default

--- app_super_gestao/database/migrations/2022_08_23_234342_create_produtos_table.php
+++ app_super_gestao/database/migrations/2022_08_23_234342_create_produtos_table.php
@@ -18,9 +18,9 @@ class CreateProdutosTable extends Migration
             $table->string('nome', 100);
             $table->text('descricao')->nullable();
             $table->integer('peso')->nullable();
-            $table->float('preco_venda', 8, 2);
-            $table->integer('estoque_minimo');
-            $table->integer('estoque_maximo');
+            $table->float('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);;
             $table->timestamps();
         });
     }
