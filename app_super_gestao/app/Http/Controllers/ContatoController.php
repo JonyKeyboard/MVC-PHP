35154a55e5f4cbf968ba01492dbfc0403f2934f6
Stanley Jony <1jony2jony@gmail.com>
2022-08-31 16:17:34 -0300

    Seção 9: Validação de campos required

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -8,28 +8,23 @@ use App\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-        /*
-        print_r($request->all());
-        echo $request->input('_token');
-        echo $request->input('email');
-
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-
-        //print_r($contato->getAttributes());
-
-        $contato->save();
-        */
-
-        $contato = new SiteContato();
-        $contato->create($request->all());
-
-        //rint_r($contato->getAttributes());
 
         return view('site.contato');
+
+    }
+
+    public function salvar(Request $request){
+
+        //realizar a validacao dos dados do formulário recebidos no request
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+
+        //SiteContato::create($request->all());
+
     }
 }
