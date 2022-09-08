48434477aae3994f81db264bd15e2293c626394d
Stanley Jony <1jony2jony@gmail.com>
2022-09-07 21:38:13 -0300

    Seção 10: Manipulando request via middleware

--- app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
+++ app_super_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,9 +13,26 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
         // verifica se o usuario possui acesso a rota
+        echo $metodo_autenticacao . ' - ' . $perfil . '<br>';
+
+        if($metodo_autenticacao == 'padrao') {
+            echo 'Verificar o usuario e senha no banco de dados ' . $perfil . '<br>';
+        }
+
+
+        if($metodo_autenticacao == 'ldap') {
+            echo 'Verificar o usuario e senha no AD ' . $perfil . '<br>';
+        }
+
+        if($$perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos';
+        } else {
+            echo 'Carregar o perfil do banco de dados';
+        }
+
         if(false){
             return $next($request);
         } else {
