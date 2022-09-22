4e5156ba3847013f214ff235f871eb74f193b8af
Stanley <1jony2jony@gmail.com>
2022-09-22 09:53:13 -0300

    Seção 12: eloquent 1 for 1 p1

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -38,6 +38,9 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
 
     //produtos
     Route::resource('produto', 'ProdutoController');
+
+    //produtos detalhes
+    Route::resource('produto-detalhe', 'ProdutoDetalheController');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
