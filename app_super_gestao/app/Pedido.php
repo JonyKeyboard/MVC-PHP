7913460ed247fad8f5fa174068e16ceb1f95f934
Stanley <1jony2jony@gmail.com>
2022-10-06 09:24:38 -0300

    Finalizando o projeto super gestão

--- app_super_gestao/app/Pedido.php
+++ app_super_gestao/app/Pedido.php
@@ -9,7 +9,7 @@ class Pedido extends Model
     public function produtos() {
         //return $this->belongsToMany('App\Produto', 'pedidos_produtos');
 
-        return $this->belongsToMany('App\Item', 'pedidos_produtos', 'pedido_id', 'produto_id')->withPivot('created_at');
+        return $this->belongsToMany('App\Item', 'pedidos_produtos', 'pedido_id', 'produto_id')->withPivot('id', 'created_at');
         /*
             1 - Modelo do relacionamento NxN em relação o Modelo que estamos implementados
             2 - É a tabela auxiliar que armazenamos registros de relacionamento
